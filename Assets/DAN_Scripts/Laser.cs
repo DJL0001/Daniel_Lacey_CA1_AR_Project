b5b2bd0ca00da1335dc62d2951a07d248cb1a253
DJL0001 <115580694+DJL0001@users.noreply.github.com>
2023-12-01 18:27:08 +0000

    User Test Build. Added music

---
+++ Assets/DAN_Scripts/Laser.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* /* [RequiredComponent(typeof(LineRenderer))] */
+public class Laser : MonoBehaviour
+{
+    [SerializeField]float laserOffTime = .5f;
+    [SerializeField]float maxDistance = 300f; 
+    LineRenderer lr;
+    bool canFire;
+
+    void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr.enabled = false;
+    }
+
+    void Update()
+    {
+        FireLaser(transform.forward * maxDistance);
+    }
+
+    public void FireLaser(Vector3 targetPosition)
+    {
+        if(canFire)
+        {
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, targetPosition);
+            lr.enabled = true;
+            canFire = false;
+        }
+        
+        Invoke("TurnOffLaser", laserOffTime);
+    }
+
+    
+
+    void TurnOffLaser()
+    {
+        lr.enabled = false;
+        canFire = true;
+    }
+
+}
